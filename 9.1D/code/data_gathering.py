b99a71d239c63b6a601d8aba511c9f2e0438cebc
Kushan-Nilanga <dknathalage@gmail.com>
2023-01-04 22:57:07 +0000

    project restructure

--- 9.1D/code/data_gathering.py
+++
@@ -1,7 +0,0 @@
-from .utils import create_artifacts_dir
-artefacts_dir = create_artifacts_dir(__file__.split('/').pop().split('.')[0])
-
-import pandas as pd
-
-def load_raw(path: str) -> pd.DataFrame:
-    return pd.read_csv(path)
--- 9.1D/code/utils.py
+++ 9.1D/code/utils.py
@@ -1,10 +1,6 @@
 import os
 
-def create_artifacts_dir(postfix):
-    artifacts_dir = os.path.join(os.getcwd(), 'artefacts')
-    if not os.path.exists(artifacts_dir):
-        os.mkdir(artifacts_dir)
-    artifacts_dir = os.path.join(artifacts_dir, postfix)
-    if not os.path.exists(artifacts_dir):
-        os.mkdir(artifacts_dir)
-    return artifacts_dir
+def create_artifacts_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
